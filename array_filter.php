d9eec0ca9de41097ba928524029ef4230c7e99b5
unknown <shalmayev1993@gmail.com>
2019-12-03 12:31:50 +0200

    added functionallity to array_filter

--- array_filter.php
+++ array_filter.php
@@ -2,17 +2,37 @@
 
 require('./functions.php');
 
-$users = [
-    ['username' => 'alex', 'score'=> 0],
-    ['username' => 'ashely', 'score'=> 0],
-    ['username' => 'or', 'score'=> 50],
-    ['username' => 'idan', 'score'=> 12]
-];
+// /** Example #1 */
+// $users = [
+//     ['username' => 'alex', 'score'=> 0],
+//     ['username' => 'ashely', 'score'=> 0],
+//     ['username' => 'or', 'score'=> 50],
+//     ['username' => 'idan', 'score'=> 12]
+// ];
 
-$filteredUsers = array_filter($users, function($u){
-    if($u['score'] > 10){
-        return true;
-    }
-});
+// $filteredUsers = array_filter($users, function($u, $i){
+    
+//     //Option #1 
+//     // if($u['score'] > 10){
+//     //     return true;
+//     // }
 
-dd($filteredUsers);
+//     // Option #2
+//     // return $u['score'] > 10;
+
+//     // Option #3
+//     return $i === 0;
+
+// }, ARRAY_FILTER_USE_BOTH);
+
+// dd($filteredUsers);
+
+
+
+
+/** Example #2 */
+$scores = [2,3,20, null, false, 0, 202];
+
+// will return all positive values and will omit all falsy values.
+$filteredScores = array_filter($scores);
+dd($filteredScores);
