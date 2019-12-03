519a60be9dcf2270e4defd3b990713c5b7ef5c16
unknown <shalmayev1993@gmail.com>
2019-12-03 12:18:23 +0200

    added example to array_filter

--- array_filter.php
+++ array_filter.php
@@ -1,2 +1,18 @@
 <?php
 
+require('./functions.php');
+
+$users = [
+    ['username' => 'alex', 'score'=> 0],
+    ['username' => 'ashely', 'score'=> 0],
+    ['username' => 'or', 'score'=> 50],
+    ['username' => 'idan', 'score'=> 12]
+];
+
+$filteredUsers = array_filter($users, function($u){
+    if($u['score'] > 10){
+        return true;
+    }
+});
+
+dd($filteredUsers);
