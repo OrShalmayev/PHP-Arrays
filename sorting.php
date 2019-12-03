a0b0f63f77b57fe7fedba85546c87f3524c54cac
unknown <shalmayev1993@gmail.com>
2019-12-03 14:11:32 +0200

    first commit for sorting

---
+++ sorting.php
@@ -0,0 +1,21 @@
+<?php
+
+// sort() - simply sort an array.
+
+// rsort(); - sort in reverse order.
+
+// asort() - sort by ascending order. // will sort an array but it will keep the indexes of the array in place 
+
+// arsort() - // will do the same as above but in a reverse order
+
+// ksort() - will sort by the key
+
+// krsort() - the same as above but in reverse order.
+
+// natsort() - will sort by natural sort algorithm
+
+// usort() - lets you to sort by a defined functions
+
+// uksort() - lets you to sort by a defined functions but with the keys
+
+// array_multisort()
