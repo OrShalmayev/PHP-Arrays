7044969447ff12aed4bbe4e9411e809a4e332b27
unknown <shalmayev1993@gmail.com>
2019-12-03 15:23:49 +0200

    added sorting file

--- sorting.php
+++ sorting.php
@@ -1,5 +1,5 @@
 <?php
-
+require('./functions.php');
 // sort() - simply sort an array.
 
 // rsort(); - sort in reverse order.
@@ -18,4 +18,60 @@
 
 // uksort() - lets you to sort by a defined functions but with the keys
 
-// array_multisort()
+// array_multisort() - sort multiple arrays side by side.
+
+/** Example: natsort() */
+// $files = ['img3.png','img6.png','img1.png','img4.png'];
+// natsort($files);
+// dd($files);
+
+
+/** Example: usort() */
+// $numbers = [4,4,10,1,52,89];
+
+// usort($numbers, function($f, $s){
+
+//     // if($f===$s) return 0;
+
+//     // if($f > $s){
+//     //     return 1;
+//     // }else{
+//     //     return -1;
+//     // }
+
+//     // Or
+//     return $f <=> $s;
+// });
+
+// dd($numbers);
+
+/** Example #2: usort() */
+
+// $users = [
+//     ['username'=>'Alex', 'reputation'=> 50, 'posts'=> 200],
+//     ['username'=>'Or', 'reputation'=> 300, 'posts'=> 150],
+//     ['username'=>'Netanel', 'reputation'=> 2, 'posts'=> 5],
+//     ['username'=>'Idan', 'reputation'=> 1, 'posts'=> 6]
+// ];
+
+// usort($users, function($f, $s){
+//     $scoreOne = $f['reputation'] + $f['posts'];
+//     $scoreTwo = $s['reputation'] + $s['posts'];
+//     // if to values equal check for highest reputation
+//     if($scoreOne===$scoreTwo){
+//         if($f['reputation'] > $s['reputation']){
+//             return 0;
+//         }else{
+//             return 1;
+//         }
+//     }
+//     return ($scoreOne > $scoreTwo) ? -1 : 1;
+// });
+
+// dd($users);
+
+
+/** Example: array_multisort() **/
+$numbers = [50,20,13,11,1];
+
+array_multisort($numbers, SORT_DESC);
